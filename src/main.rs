d5f96af93d6af85667d326baf993b393b060a060
notchum <jmrc98@gmail.com>
2023-05-04 23:56:10 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+//! image-stitcher
+//! image-stitcher
+//! image-stitcher
+//! image-stitcher
+
+use image::GenericImageView;
+
+fn main() {
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let img = image::open("/home/chum/repos/dotfiles/prose/screenshot1.png").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("test.png").unwrap();
+}
