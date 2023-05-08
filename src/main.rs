e62824b1b383460d835dc95937211229eb724380
notchum <jmrc98@gmail.com>
2023-05-07 19:38:37 -0600

    implement orientation command line args

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,28 @@ struct Orient {
 
 fn main() {
     let cli = Cli::parse();
+    let orient = &cli.orient;
 
     let imgx = image::open(cli.image_1).unwrap();
     let imgy = image::open(cli.image_2).unwrap();
 
-    println!("dimensions {:?}", imgx.dimensions());
-    println!("dimensions {:?}", imgy.dimensions());
+    println!("IMAGE_1 dimensions {:?}", imgx.dimensions());
+    println!("IMAGE_2 dimensions {:?}", imgy.dimensions());
 
-    let mut imgbuf = image::ImageBuffer::new(imgx.width(), imgx.height() + imgy.height());
-    imgbuf.copy_from(&imgx, 0, 0).unwrap();
-    imgbuf.copy_from(&imgy, 0, imgx.height()).unwrap();
+    let mut imgbuf;
+
+    if orient.horizontal {
+        imgbuf = image::ImageBuffer::new(imgx.width() + imgy.width(), imgx.height());
+        imgbuf.copy_from(&imgx, 0, 0).unwrap();
+        imgbuf.copy_from(&imgy, imgx.width(), 0).unwrap();
+    } else if orient.vertical {
+        imgbuf = image::ImageBuffer::new(imgx.width(), imgx.height() + imgy.height());
+        imgbuf.copy_from(&imgx, 0, 0).unwrap();
+        imgbuf.copy_from(&imgy, 0, imgx.height()).unwrap();
+    } else {
+        panic!("`orient` did not contain `horizontal` or `vertical`");
+    }
+
     println!("output image dimensions = {:?}", imgbuf.dimensions());
     imgbuf.save("test.png").unwrap();
 }
